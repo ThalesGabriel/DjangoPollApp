b5e0e5599a368f2179ee5584cbf290a6764e21d9
Thales Gabriel <thales.anjos@bemind.com>
2021-04-28 08:56:12 -0300

    Fluxo de votação completo

---
+++ polls/admin.py
@@ -0,0 +1,21 @@
+from django.contrib import admin
+
+from .models import Choice, Question
+
+
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['publication_date'], 'classes': ['collapse']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'publication_date', 'wasPublishedRecently')
+    list_filter = ['publication_date']
+    search_fields = ['question_text']
+
+admin.site.register(Question, QuestionAdmin)
